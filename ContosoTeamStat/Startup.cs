ce7f4838814a1448c6e79df31945994474012d07
abhilasha21 <ms.perfectionisr@rediffmail.com>
2017-02-21 06:38:55 +0100

    abcd abcd

---
+++ ContosoTeamStat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContosoTeamStat.Startup))]
+namespace ContosoTeamStat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
